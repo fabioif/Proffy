e1593c6cf64ab8e220f37faf7c973b5d08f01ef9
fabio.soares <Fabio.Soares@tcm.ba.gov.br>
2020-08-04 02:26:15 -0300

    Layout mobile e web

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import logo from '../../assets/images/logo.svg';
-import landingimg from '../../assets/images/landing.svg';
+import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHertIcon from '../../assets/images/icons/purple-heart.svg';
@@ -16,27 +16,24 @@ return (
                 <img src={logo} alt="Proffy"/>
                 <h2>Sua plataforma de estudos online.</h2>
             </div>
-            <img src={landingimg}
-             alt="Platafirna de estudos"
+            <img src={landingImg}
+             alt="Plataforma de estudos"
              />
-
             <div className="buttons-container"> 
             <a href="" className="study">
                 <img src={studyIcon} alt="Estudar"/>
+                Estudar
             </a>
-
             <a href="" className="give-classes">
-                <img src={giveClassesIcon} alt="Estudar"/>
+                <img src={giveClassesIcon} alt="Dar aulas"/>
+                Dar aulas
             </a>
             </div>
                 <span className="total-connections"> 
                 Total de 200 conexões já realizadas <img src={purpleHertIcon} alt="Coração roxo"/>
-                
                 </span>
         </div>
     </div>
-
-
 )
 }
 
